7627b45efc4219d98a0cf841c6d9843870a58b5b
Tomas Lin <tomas@secretescapes.com>
2011-10-10 16:53:16 +0100

    changed minimal grails version

--- CdnResourcesGrailsPlugin.groovy
+++ CdnResourcesGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class CdnResourcesGrailsPlugin {
   // the plugin version
   def version = "0.1"
   // the version or versions of Grails the plugin is designed for
-  def grailsVersion = "1.3.7 > *"
+  def grailsVersion = "1.3.1 > *"
 
   // the other plugins this plugin depends on
   def dependsOn = [resources:'1.0 > *']
